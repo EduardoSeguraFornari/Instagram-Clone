08aac2702da3b166268a4371f3acb1a742b3c382
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2026-01-16 09:41:36 -0300

    profile view

---
+++ Instagram-Clone/Core/Components/UserStatView.swift
@@ -0,0 +1,27 @@
+//
+//  UserStatView.swift
+//  Instagram-Clone
+//
+//  Created by Eduardo Segura Fornari on 16/01/26.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    let value: Int
+    let title: String
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            Text(title)
+                .font(.footnote)
+        }
+        .frame(width: 76)
+    }
+}
+
+#Preview {
+    UserStatView(value: 3, title: "Posts")
+}
