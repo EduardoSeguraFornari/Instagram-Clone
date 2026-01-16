2146acae411269d9c8493f9b2aae1dd3f84823a2
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2026-01-16 08:29:36 -0300

    adding .gitignore

---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by Eduardo Segura Fornari on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
