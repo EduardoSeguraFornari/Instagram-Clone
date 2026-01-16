a3cc98ce3d9696dc0494c8da6c587b38029c646f
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2026-01-16 08:49:47 -0300

    tab bar setup

---
+++ Instagram-Clone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  Instagram-Clone
+//
+//  Created by Eduardo Segura Fornari on 16/01/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
