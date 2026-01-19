ce47924d7d4df134e4ee8da802b0940b9ab73b15
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2026-01-19 15:30:53 -0300

    firebase setup

--- Instagram-Clone/App/Instagram_CloneApp.swift
+++ Instagram-Clone/App/Instagram_CloneApp.swift
@@ -5,10 +5,21 @@
 //  Created by Eduardo Segura Fornari on 16/01/26.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct Instagram_CloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
